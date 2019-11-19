095d25eb366e199c4a9ac0439eaa727b4c8221c8
Jan Lampič <jan.lampic13@gmail.com>
2019-11-19 12:31:47 +0100

    Working api module (only health check)

--- api/src/main/java/si/kuharskimojster/api/ApiApplication.java
+++ api/src/main/java/si/kuharskimojster/api/ApiApplication.java
@@ -3,10 +3,8 @@ package si.kuharskimojster.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @ComponentScan("si.kuharskimojster")
-@EnableJpaRepositories(basePackages = "si.kuharskimojster.repositories")
 @SpringBootApplication
 public class ApiApplication {
 
