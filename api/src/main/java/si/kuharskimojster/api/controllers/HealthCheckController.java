f7c06c32ce1232848bf8dff0c87e8e8eff2db121
Jan Lampič <jan.lampic13@gmail.com>
2020-01-11 11:07:24 +0100

    Update gitignore

---
+++ api/src/main/java/si/kuharskimojster/api/controllers/HealthCheckController.java
@@ -0,0 +1,15 @@
+package si.kuharskimojster.api.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import si.kuharskimojster.api.model.ResponseModel;
+
+@RestController
+public class HealthCheckController {
+    @GetMapping("/")
+    public ResponseEntity<ResponseModel> getHealth() {
+        return new ResponseEntity<>(new ResponseModel("Recipe CRUD application is running...", HttpStatus.OK.value()), HttpStatus.OK);
+    }
+}
